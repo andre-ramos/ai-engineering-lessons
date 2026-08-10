a3408f8a8279f3ce16d7c0c9958a5588b117e79f
Andre Ramos <andre.ramos.corp@gmail.com>
2026-08-10 19:18:01 -0300

    self attention class

--- llm-from-scratch/self_attention.py
+++ llm-from-scratch/self_attention.py
@@ -74,7 +74,7 @@ print(all_context_vecs)
 
 # 3.4 Implementing self-attention with trainable weights
 
-#3.41. Computing the attetion weights
+#3.41. Computing the attention weights
 # Choosing the embedding size
 x_2 = inputs[1]
 d_in = inputs.shape[1] 
@@ -112,3 +112,54 @@ context_vec_2 = attn_weights_2 @ value
 
 print(context_vec_2)
 
+#3.42. Implementing a compact SelfAttention Class
+import torch.nn as nn
+class SelfAttention_v1(nn.Module):
+
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key   = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+
+    def forward(self, x):
+        keys = x @ self.W_key
+        queries = x @ self.W_query
+        values = x @ self.W_value
+        
+        attn_scores = queries @ keys.T # omega
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+
+        context_vec = attn_weights @ values
+        return context_vec
+
+torch.manual_seed(123)
+sa_v1 = SelfAttention_v1(d_in, d_out)
+print(sa_v1(inputs))
+
+# Using Linear Layer (it has a weight matrix and bias)
+
+class SelfAttention_v2(nn.Module):
+
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+
+        context_vec = attn_weights @ values
+        return context_vec
+
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+print(sa_v2(inputs))
